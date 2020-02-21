72dc1a43ab634b70791f95e65e59ec55f2e9979a
BuildTools <unconfigured@null.spigotmc.org>
2020-02-20 20:17:27 -0600

    updates

--- src/main/java/com/kwpugh/easy_steel/util/GeneralModConfig.java
+++ src/main/java/com/kwpugh/easy_steel/util/GeneralModConfig.java
@@ -5,29 +5,29 @@ import net.minecraftforge.common.ForgeConfigSpec;
 
 public class GeneralModConfig
 {
-    public static ForgeConfigSpec.BooleanValue BORNITE_ORE_GENERATION;
-    public static ForgeConfigSpec.IntValue BORNITE_ORE_CHANCE;
-    public static ForgeConfigSpec.IntValue BORNITE_ORE_SIZE;
-    public static ForgeConfigSpec.IntValue BORNITE_ORE_MIN_HEIGHT;
-    public static ForgeConfigSpec.IntValue BORNITE_ORE_MAX_HEIGHT;
+//    public static ForgeConfigSpec.BooleanValue BORNITE_ORE_GENERATION;
+//    public static ForgeConfigSpec.IntValue BORNITE_ORE_CHANCE;
+//    public static ForgeConfigSpec.IntValue BORNITE_ORE_SIZE;
+//    public static ForgeConfigSpec.IntValue BORNITE_ORE_MIN_HEIGHT;
+//    public static ForgeConfigSpec.IntValue BORNITE_ORE_MAX_HEIGHT;
 
-    public static ForgeConfigSpec.BooleanValue CASSITERITE_ORE_GENERATION;
-    public static ForgeConfigSpec.IntValue CASSITERITE_ORE_CHANCE;
-    public static ForgeConfigSpec.IntValue CASSITERITE_ORE_SIZE;
-    public static ForgeConfigSpec.IntValue CASSITERITE_ORE_MIN_HEIGHT;
-    public static ForgeConfigSpec.IntValue CASSITERITE_ORE_MAX_HEIGHT;
+//    public static ForgeConfigSpec.BooleanValue CASSITERITE_ORE_GENERATION;
+//    public static ForgeConfigSpec.IntValue CASSITERITE_ORE_CHANCE;
+//    public static ForgeConfigSpec.IntValue CASSITERITE_ORE_SIZE;
+//    public static ForgeConfigSpec.IntValue CASSITERITE_ORE_MIN_HEIGHT;
+//    public static ForgeConfigSpec.IntValue CASSITERITE_ORE_MAX_HEIGHT;
 
-    public static ForgeConfigSpec.BooleanValue WOLFRAMITE_ORE_GENERATION;
-    public static ForgeConfigSpec.IntValue WOLFRAMITE_ORE_CHANCE;
-    public static ForgeConfigSpec.IntValue WOLFRAMITE_ORE_SIZE;
-    public static ForgeConfigSpec.IntValue WOLFRAMITE_ORE_MIN_HEIGHT;
-    public static ForgeConfigSpec.IntValue WOLFRAMITE_ORE_MAX_HEIGHT;
+//    public static ForgeConfigSpec.BooleanValue WOLFRAMITE_ORE_GENERATION;
+//    public static ForgeConfigSpec.IntValue WOLFRAMITE_ORE_CHANCE;
+//    public static ForgeConfigSpec.IntValue WOLFRAMITE_ORE_SIZE;
+//    public static ForgeConfigSpec.IntValue WOLFRAMITE_ORE_MIN_HEIGHT;
+//    public static ForgeConfigSpec.IntValue WOLFRAMITE_ORE_MAX_HEIGHT;
 
-    public static ForgeConfigSpec.BooleanValue RUTILE_ORE_GENERATION;
-    public static ForgeConfigSpec.IntValue RUTILE_ORE_CHANCE;
-    public static ForgeConfigSpec.IntValue RUTILE_ORE_SIZE;
-    public static ForgeConfigSpec.IntValue RUTILE_ORE_MIN_HEIGHT;
-    public static ForgeConfigSpec.IntValue RUTILE_ORE_MAX_HEIGHT;
+//    public static ForgeConfigSpec.BooleanValue RUTILE_ORE_GENERATION;
+//    public static ForgeConfigSpec.IntValue RUTILE_ORE_CHANCE;
+//    public static ForgeConfigSpec.IntValue RUTILE_ORE_SIZE;
+//    public static ForgeConfigSpec.IntValue RUTILE_ORE_MIN_HEIGHT;
+//    public static ForgeConfigSpec.IntValue RUTILE_ORE_MAX_HEIGHT;
     
     public static ForgeConfigSpec.BooleanValue ENABLE_BRONZE_RELATED_ITEMS;
     
@@ -43,45 +43,45 @@ public class GeneralModConfig
 
     public static void init(ForgeConfigSpec.Builder SERVER_BUILDER)
     {
-        SERVER_BUILDER.comment("Bornite Ore Generation").push("bornite_ore");
-
-        BORNITE_ORE_GENERATION = SERVER_BUILDER.comment("Generate Bornite Ore in the world [true / false]").define("borniteOreGeneration", true);
-        BORNITE_ORE_SIZE = SERVER_BUILDER.comment("Size of Bornite Ore pockets [0-100, default: 12]").defineInRange("borniteOreSize", 12, 0, 100);
-        BORNITE_ORE_CHANCE = SERVER_BUILDER.comment("Chances of Bornite Ore pocket being generated [0-100, default: 25]").defineInRange("borniteOreChance", 25, 0, 100);
-        BORNITE_ORE_MIN_HEIGHT = SERVER_BUILDER.comment("Minimal height for Bornite Ore pocket generation, [0-255, default: 1]").defineInRange("borniteOreMinHeight", 1, 0, 255);
-        BORNITE_ORE_MAX_HEIGHT = SERVER_BUILDER.comment("Maximal height for Bornite Ore pocket generation [0-255, default: 200]").defineInRange("borniteOreMaxHeight", 200, 0, 255);
-
-        SERVER_BUILDER.pop();
+//        SERVER_BUILDER.comment("Bornite Ore Generation").push("bornite_ore");
+//
+//        BORNITE_ORE_GENERATION = SERVER_BUILDER.comment("Generate Bornite Ore in the world [true / false]").define("borniteOreGeneration", true);
+//        BORNITE_ORE_SIZE = SERVER_BUILDER.comment("Size of Bornite Ore pockets [0-100, default: 12]").defineInRange("borniteOreSize", 12, 0, 100);
+//        BORNITE_ORE_CHANCE = SERVER_BUILDER.comment("Chances of Bornite Ore pocket being generated [0-100, default: 25]").defineInRange("borniteOreChance", 25, 0, 100);
+//        BORNITE_ORE_MIN_HEIGHT = SERVER_BUILDER.comment("Minimal height for Bornite Ore pocket generation, [0-255, default: 1]").defineInRange("borniteOreMinHeight", 1, 0, 255);
+//        BORNITE_ORE_MAX_HEIGHT = SERVER_BUILDER.comment("Maximal height for Bornite Ore pocket generation [0-255, default: 200]").defineInRange("borniteOreMaxHeight", 200, 0, 255);
+//
+//        SERVER_BUILDER.pop();
                     
-        SERVER_BUILDER.comment("Cassiterite Ore Generation").push("cassiterite_ore");
-
-        CASSITERITE_ORE_GENERATION = SERVER_BUILDER.comment("Generate Cassiterite Ore in the world [true / false]").define("cassiteriteOreGeneration", true);
-        CASSITERITE_ORE_SIZE = SERVER_BUILDER.comment("Size of Cassiterite Ore pockets [0-100, default: 9]").defineInRange("cassiteriteOreSize", 9, 0, 100);
-        CASSITERITE_ORE_CHANCE = SERVER_BUILDER.comment("Chances of Cassiterite Ore pocket being generated [0-100, default: 25]").defineInRange("cassiteriteOreChance", 25, 0, 100);
-        CASSITERITE_ORE_MIN_HEIGHT = SERVER_BUILDER.comment("Minimal height for Cassiterite Ore pocket generation, [0-255, default: 1]").defineInRange("cassiteriteOreMinHeight", 1, 0, 255);
-        CASSITERITE_ORE_MAX_HEIGHT = SERVER_BUILDER.comment("Maximal height for Cassiterite Ore pocket generation [0-255, default: 200]").defineInRange("cassiteriteOreMaxHeight", 200, 0, 255);
-
-        SERVER_BUILDER.pop();
+//        SERVER_BUILDER.comment("Cassiterite Ore Generation").push("cassiterite_ore");
+//
+//        CASSITERITE_ORE_GENERATION = SERVER_BUILDER.comment("Generate Cassiterite Ore in the world [true / false]").define("cassiteriteOreGeneration", true);
+//        CASSITERITE_ORE_SIZE = SERVER_BUILDER.comment("Size of Cassiterite Ore pockets [0-100, default: 9]").defineInRange("cassiteriteOreSize", 9, 0, 100);
+//        CASSITERITE_ORE_CHANCE = SERVER_BUILDER.comment("Chances of Cassiterite Ore pocket being generated [0-100, default: 25]").defineInRange("cassiteriteOreChance", 25, 0, 100);
+//        CASSITERITE_ORE_MIN_HEIGHT = SERVER_BUILDER.comment("Minimal height for Cassiterite Ore pocket generation, [0-255, default: 1]").defineInRange("cassiteriteOreMinHeight", 1, 0, 255);
+//        CASSITERITE_ORE_MAX_HEIGHT = SERVER_BUILDER.comment("Maximal height for Cassiterite Ore pocket generation [0-255, default: 200]").defineInRange("cassiteriteOreMaxHeight", 200, 0, 255);
+//
+//        SERVER_BUILDER.pop();
         
-        SERVER_BUILDER.comment("Wolframite Ore Generation").push("wolframite_ore");
-
-        WOLFRAMITE_ORE_GENERATION = SERVER_BUILDER.comment("Generate Wolframite Ore in the world [true / false]").define("wolframiteOreGeneration", true);
-        WOLFRAMITE_ORE_SIZE = SERVER_BUILDER.comment("Size of Wolframite Ore pockets [0-100, default: 5]").defineInRange("wolframiteOreSize", 4, 0, 100);
-        WOLFRAMITE_ORE_CHANCE = SERVER_BUILDER.comment("Chances of Wolframite Ore pocket being generated [0-100, default: 6]").defineInRange("wolframiteOreChance", 4, 0, 100);
-        WOLFRAMITE_ORE_MIN_HEIGHT = SERVER_BUILDER.comment("Minimal height for Wolframite Ore pocket generation, [0-255, default: 1]").defineInRange("wolframiteOreMinHeight", 1, 0, 255);
-        WOLFRAMITE_ORE_MAX_HEIGHT = SERVER_BUILDER.comment("Maximal height for Wolframite Ore pocket generation [0-255, default: 200]").defineInRange("wolframiteOreMaxHeight", 15, 0, 255);
-
-        SERVER_BUILDER.pop();
+//        SERVER_BUILDER.comment("Wolframite Ore Generation").push("wolframite_ore");
+//
+//        WOLFRAMITE_ORE_GENERATION = SERVER_BUILDER.comment("Generate Wolframite Ore in the world [true / false]").define("wolframiteOreGeneration", true);
+//        WOLFRAMITE_ORE_SIZE = SERVER_BUILDER.comment("Size of Wolframite Ore pockets [0-100, default: 5]").defineInRange("wolframiteOreSize", 4, 0, 100);
+//        WOLFRAMITE_ORE_CHANCE = SERVER_BUILDER.comment("Chances of Wolframite Ore pocket being generated [0-100, default: 6]").defineInRange("wolframiteOreChance", 4, 0, 100);
+//        WOLFRAMITE_ORE_MIN_HEIGHT = SERVER_BUILDER.comment("Minimal height for Wolframite Ore pocket generation, [0-255, default: 1]").defineInRange("wolframiteOreMinHeight", 1, 0, 255);
+//        WOLFRAMITE_ORE_MAX_HEIGHT = SERVER_BUILDER.comment("Maximal height for Wolframite Ore pocket generation [0-255, default: 200]").defineInRange("wolframiteOreMaxHeight", 15, 0, 255);
+//
+//        SERVER_BUILDER.pop();
         
-        SERVER_BUILDER.comment("Rutile Ore Generation").push("rutile");
-
-        RUTILE_ORE_GENERATION = SERVER_BUILDER.comment("Generate Rutile Ore in the world [true / false]").define("rutileOreGeneration", true);
-        RUTILE_ORE_SIZE = SERVER_BUILDER.comment("Size of Rutile Ore pockets [0-100, default: 5]").defineInRange("rutileOreSize", 4, 0, 100);
-        RUTILE_ORE_CHANCE = SERVER_BUILDER.comment("Chances of Rutile Ore pocket being generated [0-100, default: 6]").defineInRange("rutileOreChance", 4, 0, 100);
-        RUTILE_ORE_MIN_HEIGHT = SERVER_BUILDER.comment("Minimal height for Rutile Ore pocket generation, [0-255, default: 1]").defineInRange("rutileOreMinHeight", 1, 0, 255);
-        RUTILE_ORE_MAX_HEIGHT = SERVER_BUILDER.comment("Maximal height for Rutile Ore pocket generation [0-255, default: 200]").defineInRange("rutileOreMaxHeight", 15, 0, 255);
-
-        SERVER_BUILDER.pop();
+//        SERVER_BUILDER.comment("Rutile Ore Generation").push("rutile");
+//
+//        RUTILE_ORE_GENERATION = SERVER_BUILDER.comment("Generate Rutile Ore in the world [true / false]").define("rutileOreGeneration", true);
+//        RUTILE_ORE_SIZE = SERVER_BUILDER.comment("Size of Rutile Ore pockets [0-100, default: 5]").defineInRange("rutileOreSize", 4, 0, 100);
+//        RUTILE_ORE_CHANCE = SERVER_BUILDER.comment("Chances of Rutile Ore pocket being generated [0-100, default: 6]").defineInRange("rutileOreChance", 4, 0, 100);
+//        RUTILE_ORE_MIN_HEIGHT = SERVER_BUILDER.comment("Minimal height for Rutile Ore pocket generation, [0-255, default: 1]").defineInRange("rutileOreMinHeight", 1, 0, 255);
+//        RUTILE_ORE_MAX_HEIGHT = SERVER_BUILDER.comment("Maximal height for Rutile Ore pocket generation [0-255, default: 200]").defineInRange("rutileOreMaxHeight", 15, 0, 255);
+//
+//        SERVER_BUILDER.pop();
         
         SERVER_BUILDER.comment("Enable the Crack Hammer's right-click ability").push("crack_hammer_ability");
         
--- src/main/java/com/kwpugh/easy_steel/world/OreGenerator.java
+++ src/main/java/com/kwpugh/easy_steel/world/OreGenerator.java
@@ -1,7 +1,7 @@
 package com.kwpugh.easy_steel.world;
 
-import com.kwpugh.easy_steel.util.GeneralModConfig;
 import com.kwpugh.easy_steel.lists.BlockList;
+import com.kwpugh.easy_steel.util.NewConfig;
 
 import net.minecraft.world.biome.Biome;
 import net.minecraft.world.gen.GenerationStage;
@@ -17,18 +17,32 @@ public class OreGenerator
     public static void setupOregen()
     {
         for(Biome biome : ForgeRegistries.BIOMES)
-        {        	
-            if(GeneralModConfig.BORNITE_ORE_GENERATION.get())
-            	biome.addFeature(GenerationStage.Decoration.UNDERGROUND_ORES, Feature.ORE.withConfiguration(new OreFeatureConfig(OreFeatureConfig.FillerBlockType.NATURAL_STONE, BlockList.bornite_ore.getDefaultState(), GeneralModConfig.BORNITE_ORE_SIZE.get())).withPlacement(Placement.COUNT_RANGE.configure(new CountRangeConfig(GeneralModConfig.BORNITE_ORE_CHANCE.get(), GeneralModConfig.BORNITE_ORE_MIN_HEIGHT.get(), GeneralModConfig.BORNITE_ORE_MIN_HEIGHT.get(), GeneralModConfig.BORNITE_ORE_MAX_HEIGHT.get()))));
-            
-            if(GeneralModConfig.CASSITERITE_ORE_GENERATION.get())
-        		 biome.addFeature(GenerationStage.Decoration.UNDERGROUND_ORES, Feature.ORE.withConfiguration(new OreFeatureConfig(OreFeatureConfig.FillerBlockType.NATURAL_STONE, BlockList.cassiterite_ore.getDefaultState(), GeneralModConfig.CASSITERITE_ORE_SIZE.get())).withPlacement(Placement.COUNT_RANGE.configure(new CountRangeConfig(GeneralModConfig.CASSITERITE_ORE_CHANCE.get(), GeneralModConfig.CASSITERITE_ORE_MIN_HEIGHT.get(), GeneralModConfig.CASSITERITE_ORE_MIN_HEIGHT.get(), GeneralModConfig.CASSITERITE_ORE_MAX_HEIGHT.get()))));  
-            
-            if(GeneralModConfig.WOLFRAMITE_ORE_GENERATION.get())
-            	biome.addFeature(GenerationStage.Decoration.UNDERGROUND_ORES, Feature.ORE.withConfiguration(new OreFeatureConfig(OreFeatureConfig.FillerBlockType.NATURAL_STONE, BlockList.wolframite_ore.getDefaultState(), GeneralModConfig.WOLFRAMITE_ORE_SIZE.get())).withPlacement(Placement.COUNT_RANGE.configure(new CountRangeConfig(GeneralModConfig.WOLFRAMITE_ORE_CHANCE.get(), GeneralModConfig.WOLFRAMITE_ORE_MIN_HEIGHT.get(), GeneralModConfig.WOLFRAMITE_ORE_MIN_HEIGHT.get(), GeneralModConfig.WOLFRAMITE_ORE_MAX_HEIGHT.get()))));
+        {  
+        	if(NewConfig.Server.bornite_ore_generation)
+            	biome.addFeature(GenerationStage.Decoration.UNDERGROUND_ORES, Feature.ORE.withConfiguration(new OreFeatureConfig(OreFeatureConfig.FillerBlockType.NATURAL_STONE, BlockList.bornite_ore.getDefaultState(), NewConfig.Server.bornite_ore_size)).withPlacement(Placement.COUNT_RANGE.configure(new CountRangeConfig(NewConfig.Server.bornite_ore_chance, NewConfig.Server.bornite_ore_min_height, NewConfig.Server.bornite_ore_min_height, NewConfig.Server.bornite_ore_max_height))));
+                      
+            if(NewConfig.Server.cassiterite_ore_generation)
+        		 biome.addFeature(GenerationStage.Decoration.UNDERGROUND_ORES, Feature.ORE.withConfiguration(new OreFeatureConfig(OreFeatureConfig.FillerBlockType.NATURAL_STONE, BlockList.cassiterite_ore.getDefaultState(), NewConfig.Server.bornite_ore_size)).withPlacement(Placement.COUNT_RANGE.configure(new CountRangeConfig(NewConfig.Server.cassiterite_ore_chance, NewConfig.Server.cassiterite_min_height, NewConfig.Server.cassiterite_min_height, NewConfig.Server.cassiterite_max_height))));  
 
-            if(GeneralModConfig.RUTILE_ORE_GENERATION.get())
-            	biome.addFeature(GenerationStage.Decoration.UNDERGROUND_ORES, Feature.ORE.withConfiguration(new OreFeatureConfig(OreFeatureConfig.FillerBlockType.NATURAL_STONE, BlockList.rutile_ore.getDefaultState(), GeneralModConfig.RUTILE_ORE_SIZE.get())).withPlacement(Placement.COUNT_RANGE.configure(new CountRangeConfig(GeneralModConfig.RUTILE_ORE_CHANCE.get(), GeneralModConfig.RUTILE_ORE_MIN_HEIGHT.get(), GeneralModConfig.RUTILE_ORE_MIN_HEIGHT.get(), GeneralModConfig.RUTILE_ORE_MAX_HEIGHT.get()))));
+            if(NewConfig.Server.wolframite_ore_generation)
+            	biome.addFeature(GenerationStage.Decoration.UNDERGROUND_ORES, Feature.ORE.withConfiguration(new OreFeatureConfig(OreFeatureConfig.FillerBlockType.NATURAL_STONE, BlockList.wolframite_ore.getDefaultState(), NewConfig.Server.wolframite_ore_size)).withPlacement(Placement.COUNT_RANGE.configure(new CountRangeConfig(NewConfig.Server.wolframite_ore_chance, NewConfig.Server.wolframite_min_height, NewConfig.Server.wolframite_min_height, NewConfig.Server.wolframite_max_height))));
+
+            if(NewConfig.Server.rutile_ore_generation)
+            	biome.addFeature(GenerationStage.Decoration.UNDERGROUND_ORES, Feature.ORE.withConfiguration(new OreFeatureConfig(OreFeatureConfig.FillerBlockType.NATURAL_STONE, BlockList.rutile_ore.getDefaultState(), NewConfig.Server.rutile_ore_size)).withPlacement(Placement.COUNT_RANGE.configure(new CountRangeConfig(NewConfig.Server.rutile_ore_chance, NewConfig.Server.rutile_min_height, NewConfig.Server.rutile_min_height, NewConfig.Server.rutile_max_height))));
+      
+            
+//        	if(GeneralModConfig.BORNITE_ORE_GENERATION.get())
+//	    	  	biome.addFeature(GenerationStage.Decoration.UNDERGROUND_ORES, Feature.ORE.withConfiguration(new OreFeatureConfig(OreFeatureConfig.FillerBlockType.NATURAL_STONE, BlockList.bornite_ore.getDefaultState(), GeneralModConfig.BORNITE_ORE_SIZE.get())).withPlacement(Placement.COUNT_RANGE.configure(new CountRangeConfig(GeneralModConfig.BORNITE_ORE_CHANCE.get(), GeneralModConfig.BORNITE_ORE_MIN_HEIGHT.get(), GeneralModConfig.BORNITE_ORE_MIN_HEIGHT.get(), GeneralModConfig.BORNITE_ORE_MAX_HEIGHT.get()))));
+//    
+//    	    if(GeneralModConfig.CASSITERITE_ORE_GENERATION.get())
+//				 biome.addFeature(GenerationStage.Decoration.UNDERGROUND_ORES, Feature.ORE.withConfiguration(new OreFeatureConfig(OreFeatureConfig.FillerBlockType.NATURAL_STONE, BlockList.cassiterite_ore.getDefaultState(), GeneralModConfig.CASSITERITE_ORE_SIZE.get())).withPlacement(Placement.COUNT_RANGE.configure(new CountRangeConfig(GeneralModConfig.CASSITERITE_ORE_CHANCE.get(), GeneralModConfig.CASSITERITE_ORE_MIN_HEIGHT.get(), GeneralModConfig.CASSITERITE_ORE_MIN_HEIGHT.get(), GeneralModConfig.CASSITERITE_ORE_MAX_HEIGHT.get()))));  
+//        
+//  	    if(GeneralModConfig.WOLFRAMITE_ORE_GENERATION.get())
+//      	  	biome.addFeature(GenerationStage.Decoration.UNDERGROUND_ORES, Feature.ORE.withConfiguration(new OreFeatureConfig(OreFeatureConfig.FillerBlockType.NATURAL_STONE, BlockList.wolframite_ore.getDefaultState(), GeneralModConfig.WOLFRAMITE_ORE_SIZE.get())).withPlacement(Placement.COUNT_RANGE.configure(new CountRangeConfig(GeneralModConfig.WOLFRAMITE_ORE_CHANCE.get(), GeneralModConfig.WOLFRAMITE_ORE_MIN_HEIGHT.get(), GeneralModConfig.WOLFRAMITE_ORE_MIN_HEIGHT.get(), GeneralModConfig.WOLFRAMITE_ORE_MAX_HEIGHT.get())))); 
+//            
+//          if(GeneralModConfig.RUTILE_ORE_GENERATION.get())
+//            	biome.addFeature(GenerationStage.Decoration.UNDERGROUND_ORES, Feature.ORE.withConfiguration(new OreFeatureConfig(OreFeatureConfig.FillerBlockType.NATURAL_STONE, BlockList.rutile_ore.getDefaultState(), GeneralModConfig.RUTILE_ORE_SIZE.get())).withPlacement(Placement.COUNT_RANGE.configure(new CountRangeConfig(GeneralModConfig.RUTILE_ORE_CHANCE.get(), GeneralModConfig.RUTILE_ORE_MIN_HEIGHT.get(), GeneralModConfig.RUTILE_ORE_MIN_HEIGHT.get(), GeneralModConfig.RUTILE_ORE_MAX_HEIGHT.get()))));
+  
         }
     }
 }
