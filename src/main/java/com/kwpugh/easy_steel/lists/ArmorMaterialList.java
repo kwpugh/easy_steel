819f02e52eb511aac9930b9ac3dc1c59cfc522f9
BuildTools <unconfigured@null.spigotmc.org>
2020-03-01 16:04:29 -0600

    UPDATES

--- src/main/java/com/kwpugh/easy_steel/lists/ArmorMaterialList.java
+++ src/main/java/com/kwpugh/easy_steel/lists/ArmorMaterialList.java
@@ -10,14 +10,15 @@ import net.minecraft.item.crafting.Ingredient;
 import net.minecraft.util.ResourceLocation;
 import net.minecraft.util.SoundEvent;
 
+	
 public enum ArmorMaterialList implements IArmorMaterial
 {
 	//Armor order: helmet, leggings, chestplate, boots
-	bronze("armor_bronze", 14, new int[] {2, 5, 6, 2}, 15, ItemInit.BRONZE_PLATE.get(), "entity.wolf.growl", 0.5f),
-	steel("armor_steel", 19, new int[] {3, 6, 7, 3}, 15, ItemInit.STEEL_PLATE.get(), "entity.cat.purreow", 1.0f),
-	titanium("armor_titanium", 22, new int[] {3, 6, 7, 3}, 15, ItemInit.TITANIUM_PLATE.get(), "entity.cat.purreow", 1.25f),
-	hardened_steel("armor_hardened_steel", 23, new int[] {3, 6, 7, 3}, 15, ItemInit.HARDENED_STEEL_PLATE.get(), "entity.elder_guardian.curse", 1.5f),
-	tungsten_carbide("armor_tungsten_carbide", 25, new int[] {3, 6, 8, 3}, 15, ItemInit.TUNGSTEN_CARBIDE_PLATE.get(), "entity.ender_dragon.growl", 1.75f);
+	BRONZE("armor_bronze", 14, new int[] {2, 5, 6, 2}, 15, ItemInit.BRONZE_PLATE.get(), "entity.wolf.growl", 0.5f),
+	STEEL("armor_steel", 19, new int[] {3, 6, 7, 3}, 15, ItemInit.STEEL_PLATE.get(), "entity.cat.purreow", 1.0f),
+	TITANIUM("armor_titanium", 22, new int[] {3, 6, 7, 3}, 15, ItemInit.TITANIUM_PLATE.get(), "entity.cat.purreow", 1.25f),
+	HARDENED_STEEL("armor_hardened_steel", 23, new int[] {3, 6, 7, 3}, 15, ItemInit.HARDENED_STEEL_PLATE.get(), "entity.elder_guardian.curse", 1.5f),
+	TUNGSTEN_CARBIDE("armor_tungsten_carbide", 25, new int[] {3, 6, 8, 3}, 15, ItemInit.TUNGSTEN_CARBIDE_PLATE.get(), "entity.ender_dragon.growl", 1.75f);
 
 	private static final int[] max_damage_array = new int[]{13, 15, 16, 11};
 	
